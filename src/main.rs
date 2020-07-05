72b925c0ebe673e4aee82333fcf840a2f7135b34
andrewrds <andrew@dingwall-smith.com>
2020-07-05 15:03:27 +0100

    Save login token for reuse so login rate limits are not triggered

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::env;
@@ -15,8 +17,16 @@ fn main() {
 
     let client = Client::new();
 
-    let token = login(&client, settings["username"].as_str().unwrap(), settings["password"].as_str().unwrap());
-    let product_json = retrieve_products_json(&client, &token);
+    let mut token = load_token().unwrap_or_else(|| {
+        login(&client, &settings)
+    });
+
+    let product_json = retrieve_products_json(&client, &token).unwrap_or_else(|_| {
+        // If retrieving product JSON fails, the login token may have expired so try logging in again
+        token = login(&client, &settings);
+        retrieve_products_json(&client, &token).unwrap()
+    });
+
     let heating_object = find_heating_object(&product_json);
 
     if args.len() == 1 {
@@ -25,8 +35,6 @@ fn main() {
         let target_temp = args[1].parse::<f64>().unwrap();
         set_target_temp(&client, &heating_object, &token, target_temp);
     }
-
-    logout(&client, &token);
 }
 
 fn load_settings() -> TomlValue {
@@ -36,13 +44,36 @@ fn load_settings() -> TomlValue {
     let path = path.as_path();
 
     let mut file = File::open(path)
-        .expect("Failed to open .hive-heat settings");
+        .expect("Failed to open ~/.hive-heat/conf.toml");
     let mut settings_string = String::new();
     file.read_to_string(&mut settings_string).unwrap();
     return settings_string.parse::<TomlValue>().unwrap();
 }
 
-fn login(client: &Client, username: &str, password: &str) -> String {
+fn load_token() -> Option<String> {
+    let mut path = dirs::home_dir().unwrap();
+    path.push(".hive-heat");
+    path.push("token");
+    let path = path.as_path();
+
+    match File::open(path) {
+        Ok(mut file)  => {
+	    let mut token = String::new();
+            file.read_to_string(&mut token).unwrap();
+            Some(token)
+        }
+        Err(_) => None,
+    }
+}
+
+fn login(client: &Client, settings: &TomlValue) -> String {
+    let token = send_login_request(&client, settings["username"].as_str().unwrap(), settings["password"].as_str().unwrap());
+    // Save the login token so it can be used again, otherwise login rate limits will be hit
+    save_token(&token);
+    token
+}
+
+fn send_login_request(client: &Client, username: &str, password: &str) -> String {
     let resp: Value = client
         .post(&format!("{}global/login", HIVE_API_ENDPOINT))
         .body(format!("{{\"username\":\"{}\",\"password\":\"{}\",\"devices\":true,\"products\":true,\"actions\":true,\"homes\":true}}", username, password))
@@ -55,23 +86,26 @@ fn login(client: &Client, username: &str, password: &str) -> String {
     return String::from(token);
 }
 
-fn logout(client: &Client, token: &str) {
-    client
-        .delete(&format!("{}auth/logout", HIVE_API_ENDPOINT))
-        .header(AUTHORIZATION, HeaderValue::from_str(token).unwrap())
-        .body("{}")
-        .send()
-        .expect("Logout failed");
+fn save_token(token: &str) {
+    let mut path = dirs::home_dir().unwrap();
+    path.push(".hive-heat");
+    path.push("token");
+    let path = path.as_path();
+
+    fs::write(path, token)
+        .expect("Failed to write to ~/.hive-heat/token");
 }
 
-fn retrieve_products_json(client: &Client, token: &str) -> Value {
-    return client
+
+fn retrieve_products_json(client: &Client, token: &str) -> Result<Value, Box<dyn Error>> {
+    let product_json = client
         .get(&format!("{}products?after=", HIVE_API_ENDPOINT))
-        .header(AUTHORIZATION, HeaderValue::from_str(token).unwrap())
-        .send()
-        .expect("Products request failed")
+        .header(AUTHORIZATION, HeaderValue::from_str(token)?)
+        .send()?
         .json()
         .expect("Failed to parse products response");
+
+    Ok(product_json)
 }
 
 fn find_heating_object(product_json: &Value) -> &Map<String, Value> {
